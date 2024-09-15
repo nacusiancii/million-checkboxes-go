cf758da3a79350b79e43559bf7baea54d2e856e1
nacusiancii <146810142+nacusiancii@users.noreply.github.com>
2024-09-15 17:32:11 +0530

    Update practice.go

--- practice.go
+++ practice.go
@@ -1,1 +1,13 @@
 package main
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the home page!")
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello, %s!", name)
+}
