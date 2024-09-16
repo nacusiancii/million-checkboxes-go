82cdef872dd5af1925e83ca188dad05b09a0cf3f
nacusiancii <146810142+nacusiancii@users.noreply.github.com>
2024-09-16 13:04:52 +0530

    Update practice.go

--- practice.go
+++ practice.go
@@ -15,3 +15,24 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Hello, %s!", name)
 }
+
+//This is a go comment
+/*
+ * This is a multiline comment
+ */
+func handleAreaOfRectangle(w http.responseWriter, *http.Request){
+	length := 8
+	breadth := 4
+	area := length*breadth
+	/*
+ 	 *String formating:
+   	 *%d->int
+	 *%t->boolean
+	 *%s->string
+  	 *%f->float
+	 */
+ 	fmt.Printf("Area of Rectangle: %d\n", area)
+	var isLargeRectangle bool = (area>=200)
+	fmt.Printf("Large Rectangle: %t\n",isLargeRectangle)
+	return area
+}
